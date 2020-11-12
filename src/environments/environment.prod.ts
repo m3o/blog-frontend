aa6e151463ec34fa8906cdb1656b6aabccf11717
Janos Dobronszki <dobronszki@gmail.com>
2020-11-12 12:20:49 +0100

    Add images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   m3oAddress: "https://api.m3o.dev",
   // Replace this before deploying with an actual namespace
-  m3oNamespace: "micro"
+  m3oNamespace: "concert-celtic-uncover"
 };
