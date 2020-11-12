506e7cdbcc0c32849c0bbde96994f7133eb77a1f
Janos Dobronszki <dobronszki@gmail.com>
2020-11-12 12:21:44 +0100

    Revert

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   m3oAddress: "https://api.m3o.dev",
   // Replace this before deploying with an actual namespace
-  m3oNamespace: "concert-celtic-uncover"
+  m3oNamespace: "micro"
 };
